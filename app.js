c32a206bc51fa0c9df3c216eaee04a022a54b5d0
SuYoung <golborneville@gmail.com>
2020-02-26 01:05:14 +0900

    project server set up

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import createError from 'http-errors'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import v1Route from './routes/v1'
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/v1', v1Route);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+    next(createError(404))
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+    let apiError = err;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    if (!err.status) {
+        apiError = createError(err)
+    }
+
+// set locals, only providing error in development
+    res.locals.message = apiError.message;
+    res.locals.error = process.env.NODE_ENV === 'development' ? apiError : {};
+
+// render the error page
+    return res.status(apiError.status)
+        .json({message: apiError.message})
 });
 
+// bin/www 를 그대로 사용하기 위해서 예외적으로 commonJs 문법을 적용
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express'
+import {index} from '../controllers/index.controller'
+
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', index)
 
-module.exports = router;
+export default router
